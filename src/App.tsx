7a3964622da0fac17fe3c47d78d741af90dadacb
JavierAChacon <alejandro.chr@outlook.es>
2024-12-29 15:03:19 -0400

    feat: add ScrollToTop function

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,32 @@ import ClientLayout from "./components/client/ClientLayout"
 import Laptop from "./pages/client/Laptop"
 import Cart from "./pages/client/Cart"
 import Search from "./pages/client/Search"
+import ScrollToTop from "./utils/scrollToTop"
 
 function App() {
   return (
-    <Routes>
-      {/* Client */}
-      <Route element={<ClientLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/laptop/:id/:configuration" element={<Laptop />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/search" element={<Search />}>
-          <Route path=":option" element={<Search />} />
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<ClientLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/laptop/:id/:configuration" element={<Laptop />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/search" element={<Search />}>
+            <Route path=":option" element={<Search />} />
+          </Route>
         </Route>
-      </Route>
 
-      {/* Admin */}
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<Index />} />
-        <Route path="/admin/add-laptop" element={<AddLaptop />} />
-      </Route>
+        {/* Admin */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Index />} />
+          <Route path="/admin/add-laptop" element={<AddLaptop />} />
+        </Route>
 
-      {/* Auth */}
-      <Route path="/auth/admin" element={<AdminLogin />} />
-    </Routes>
+        {/* Auth */}
+        <Route path="/auth/admin" element={<AdminLogin />} />
+      </Routes>
+    </>
   )
 }
 
